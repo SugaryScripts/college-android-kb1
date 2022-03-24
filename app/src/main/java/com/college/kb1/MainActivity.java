d8d8921fec41c13c9f87eec13d7ab150f53f39aa
Fikry Farenza <vickyfarenza@gmail.com>
2022-03-24 16:24:53 +0700

    Done functionality :sparkles:

--- app/src/main/java/com/college/kb1/MainActivity.java
+++ app/src/main/java/com/college/kb1/MainActivity.java
@@ -3,6 +3,7 @@ package com.college.kb1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Toast;
 
@@ -27,6 +28,8 @@ public class MainActivity extends AppCompatActivity {
     private int operator = 0;
 
     private int key = 0;
+    private boolean gameIsPlaying = true;
+    private boolean chance = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,20 +48,19 @@ public class MainActivity extends AppCompatActivity {
         binding.tvP1Score.setText(String.valueOf(p1Score));
         binding.tvP2Score.setText(String.valueOf(p2Score));
 
+        /* Radio buttons */
         binding.radioEasy.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 toggleDifficulty(true, false, false);
             }
         });
-
         binding.radioMedium.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 toggleDifficulty(false, true, false);
             }
         });
-
         binding.radioDifficult.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -73,6 +75,46 @@ public class MainActivity extends AppCompatActivity {
             }
         });*/
 
+
+        /* Options P1 */
+        binding.btnP1Top.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                playerAnswer(1, binding.btnP1Top.getText().toString());
+            }
+        });
+        binding.btnP1Mid.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                playerAnswer(1, binding.btnP1Mid.getText().toString());
+            }
+        });
+        binding.btnP1Bot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                playerAnswer(1, binding.btnP1Bot.getText().toString());
+            }
+        });
+
+        /* Options P2 */
+        binding.btnP2Top.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                playerAnswer(2, binding.btnP2Top.getText().toString());
+            }
+        });
+        binding.btnP2Mid.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                playerAnswer(2, binding.btnP2Mid.getText().toString());
+            }
+        });
+        binding.btnP2Bot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                playerAnswer(2, binding.btnP2Bot.getText().toString());
+            }
+        });
     }
 
     private void generateQuiz() {
@@ -164,5 +206,96 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+
+    private void playerAnswer(int player, String answerText) {
+        int answer = Integer.parseInt(answerText);
+        switch (player){
+            case 1:
+                if (answer != key){
+                    if (chance){
+                        chance = false;
+                        Toast.makeText(this, "Yah Semua Salah! ^v^", Toast.LENGTH_SHORT).show();
+                        gameIsPlaying = false;
+                    }else {
+                        chance = true;
+                        toggleButton(1, false);
+                        Toast.makeText(this, "Player 1 Salah! x_x", Toast.LENGTH_SHORT).show();
+                        gameIsPlaying = true;
+                    }
+                } else {
+                    chance = false;
+                    toggleButton(2, false);
+                    p1Score += 1;
+                    Toast.makeText(this, "Yay! Player 1 Betul! ^_^", Toast.LENGTH_SHORT).show();
+                    gameIsPlaying = false;
+                }
+                break;
+            case 2:
+                if (answer != key){
+                    if (chance){
+                        chance = false;
+                        Toast.makeText(this, "Yah Semua Salah! ^v^", Toast.LENGTH_SHORT).show();
+                        gameIsPlaying = false;
+                    } else {
+                        chance = true;
+                        toggleButton(2, false);
+                        Toast.makeText(this, "Player 2 Salah! x_x", Toast.LENGTH_SHORT).show();
+                        gameIsPlaying = true;
+                    }
+                } else {
+                    chance = false;
+                    toggleButton(1, false);
+                    p2Score += 1;
+                    Toast.makeText(this, "Yay! Player 2 Betul! ^_^", Toast.LENGTH_SHORT).show();
+                    gameIsPlaying = false;
+                }
+                break;
+        }
+
+        binding.tvP1Score.setText(String.valueOf(p1Score));
+        binding.tvP2Score.setText(String.valueOf(p2Score));
+
+        if (!gameIsPlaying){
+            new Handler().postDelayed(new Runnable() {
+                @Override public void run() {
+                    Toast.makeText(MainActivity.this, "Pertanyaan selanjutnya dalam 3 detik", Toast.LENGTH_SHORT).show();
+                }
+            }, 1000);
+            new Handler().postDelayed(new Runnable() {
+                @Override public void run() {
+                    Toast.makeText(MainActivity.this, "Pertanyaan selanjutnya dalam 2 detik", Toast.LENGTH_SHORT).show();
+                }
+            }, 2000);
+            new Handler().postDelayed(new Runnable() {
+                @Override public void run() {
+                    Toast.makeText(MainActivity.this, "Pertanyaan selanjutnya dalam 1 detik", Toast.LENGTH_SHORT).show();
+                }
+            }, 3000);
+            new Handler().postDelayed(new Runnable() {
+                @Override public void run() {
+                    gameIsPlaying = true;
+                    toggleButton(1, true);
+                    toggleButton(2, true);
+                    generateQuiz();
+                }
+            }, 4000);
+        }
+
+    }
+
+    private void toggleButton(int player, boolean enabled){
+        switch (player){
+            case 1:
+                binding.btnP1Top.setEnabled(enabled);
+                binding.btnP1Mid.setEnabled(enabled);
+                binding.btnP1Bot.setEnabled(enabled);
+                break;
+            case 2:
+                binding.btnP2Top.setEnabled(enabled);
+                binding.btnP2Mid.setEnabled(enabled);
+                binding.btnP2Bot.setEnabled(enabled);
+                break;
+        }
+    }
 
 }
